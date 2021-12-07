103daeaa5bb599f667ca0c16616ed15cd21aebda
Sarthak Verma <sarthakvdev@gmail.com>
2021-12-07 19:45:12 +0530

    Wallet connection + Assets deployed to Metaplex

---
+++ README.md
@@ -0,0 +1,8 @@
+# Solana NFT Drop Project
+### Clone and get started 👋
+To get started with this course, clone this repo and follow these commands:
+
+1. cd into the `app` folder
+2. Run `npm install` at the root of your directory
+3. Run `npm run start` to start the project
+4. Start coding!
---
+++ app/src/App.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+
+// Constants
+const TWITTER_HANDLE = "0xSarthak";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+
+          // solana.connect allows us to connect out app to the wallet
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+
+          // storing wallet's public key aka address to state
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("⚠️ Phantom wallet not found! Setup one to continue.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if(solana) {
+      const response = await solana.connect();
+      console.log("Connected w/ Public Key: ", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    } else {
+
+    }
+
+  };
+
+  // components
+  const RenderNotConnectedContainer = () => (
+    <button 
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Drop</p>
+          <p className="sub-text">NFT drop machine with fair mint</p>
+          { !walletAddress && <RenderNotConnectedContainer />}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
